6b2ac11abd369c8603f28a950bf74f633b29c347
Ming Xiong <iona.m.xiong@gmail.com>
2021-07-23 23:55:53 +0300

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,19 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filtering, setFiltering] = useState("");
+
+  const handleFiltering = (e) => {
+    setFiltering(e.target.value);
+    console.log(e.target.value);
+  };
 
   const handleNewName = (e) => {
     const name = e.target.value;
@@ -31,12 +40,15 @@ const App = () => {
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filtering} onChange={handleFiltering} />
+      </div>
       <form>
         <div>
           name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNewNumber}></input>
+          number: <input value={newNumber} onChange={handleNewNumber} />
         </div>
         <div>
           <button type="submit" onClick={(event) => addInfo(event)}>
@@ -45,11 +57,21 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <p key={i}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      {filtering === ""
+        ? persons.map((person) => (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          ))
+        : persons
+            .filter((person) =>
+              person.name.toLowerCase().includes(filtering.toLowerCase())
+            )
+            .map((person) => (
+              <p key={person.name}>
+                {person.name} {person.number}
+              </p>
+            ))}
     </>
   );
 };
