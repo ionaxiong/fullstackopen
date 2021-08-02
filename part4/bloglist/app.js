1bd800ad008423d1a9d00d67777afda03f6ebacd
Ming Xiong <iona.m.xiong@gmail.com>
2021-08-02 21:17:37 +0300

    refactor into models module, add request module

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,43 +1,42 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-app.use(express.json())
-app.use(cors())
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog")
+const logger = require("./utils/logger");
+app.use(express.json());
+app.use(cors());
 
-const blogSchema = new mongoose.Schema({
-	author: String,
-	title: String,
-	url: String,
-	likes: Number
-})
+logger.info("connecting to", config.MONGODB_URL);
 
-const Blog = mongoose.model('Blog', blogSchema)
+mongoose
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
-const url = config.MONGODB_URL
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-mongoose.connect(url, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-	useCreateIndex: true,
-})
+app.post("/api/blogs", (request, response) => {
+  console.log(request);
+  const blog = new Blog(request.body);
 
-app.get('/api/blogs', (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs)
-	})
-})
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
 
-app.post('/api/blogs', (request, response) => {
-	console.log(request)
-	const blog = new Blog(request.body)
-
-
-	blog.save().then((result) => {
-		response.status(201).json(result)
-	})
-})
-
-module.exports = app
+module.exports = app;
