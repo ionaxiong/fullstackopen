def0597be1ae2114df5c6fd27c44537c8640aa19
Ming Xiong <iona.m.xiong@gmail.com>
2021-08-02 18:54:00 +0300

    update 4.1

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,46 +1,48 @@
-require("dotenv").config();
-const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
+require('dotenv').config()
+const express = require('express')
+const { request, response } = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+app.use(express.json())
+app.use(cors())
 
 const blogSchema = new mongoose.Schema({
-  author: String,
-  title: String,
-  url: String,
-  likes: Number,
-});
+	author: String,
+	title: String,
+	url: String,
+	likes: Number
+})
 
-const Blog = mongoose.model("Blog", blogSchema);
 
-const url = process.env.MONGODB_URL;
+const Blog = mongoose.model('Blog', blogSchema)
+
+const url = process.env.MONGODB_URL
 
 mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+})
 
-app.use(cors());
-app.use(express.json());
+app.get('/api/blogs', (request, response) => {
+	Blog.find({}).then((blogs) => {
+		response.json(blogs)
+	})
+})
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
+app.post('/api/blogs', (request, response) => {
+	console.log(request)
+	const blog = new Blog(request.body)
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+	blog.save().then((result) => {
+		response.status(201).json(result)
+	})
+})
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+	console.log(`Server running on port ${PORT}`)
+})
