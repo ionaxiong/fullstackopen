2e55a6dda78a3db2aada5ce6374bc7f0ed2b8721
Ming Xiong <iona.m.xiong@gmail.com>
2021-08-02 22:14:33 +0300

    defactor routes into controllers module, add request tests

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -9,10 +9,59 @@ blogsRouter.get("/", (request, response) => {
 });
 
 // get data by id
-blogsRouter.get('/:id', (request, response, next) => {
-    
-})
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog.toJSON());
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
+blogsRouter.post("/", (request, response) => {
+  const body = request.body;
 
+  const blog = new Blog({
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+  });
 
-module.exports = blogsRouter
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.json(savedBlog.toJSON());
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.delete("/:id", (request, response, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.put("/:id", (request, response) => {
+  const body = request.body;
+
+  const blog = {
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog.toJSON());
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
