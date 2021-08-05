105b6b76f5b06d83014b935d48b6110658c2015c
Ming Xiong <iona.m.xiong@gmail.com>
2021-08-05 18:10:54 +0300

    4.4

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -1,8 +1,10 @@
 const listHelper = require("../utils/list_helper");
 
-test("dummy return one", () => {
-  const blogs = [];
+describe("dummy", () => {
+  test("dummy return one", () => {
+    const blogs = [];
 
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+  });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.some((blog) => typeof(blog.likes) !== "number")) {
+    throw "likes is not a number!";
+  }
+  if (blogs.length === 0) {
+    return 0;
+  }
+  const likes = blogs
+    .map((blog) => blog.likes)
+    .reduce((sum, likes) => sum + likes);
+
+  return likes;
+};
+
 module.exports = {
   dummy,
+  totalLikes
 };
