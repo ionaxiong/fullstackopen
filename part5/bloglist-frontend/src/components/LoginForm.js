9586a3d11769f791537549d576bf9de973991da5
Ming Xiong <iona.m.xiong@gmail.com>
2021-08-22 15:04:59 +0300

    define prop-types requirements

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,6 +1,15 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 const LoginForm = (props) => {
+  LoginForm.propTypes = {
+    handleLogin: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    handleUsernameChange: PropTypes.func.isRequired,
+    password: PropTypes.string.isRequired,
+    handlePasswordChange: PropTypes.func.isRequired,
+  }
+
   return (
     <div>
         <h2>Login</h2>
